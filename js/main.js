f9674b18f818b7e0bacab161032a152b1c115c89
nazarroshchuk <nazarroshchuk@gmail.com>
2020-03-16 19:42:54 +0200

    add popup

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ $(function () {
         arrows: false,
         dots: true,
         fade: true,
-        // autoplay: 3000,
-
+        autoplay: 3000,
     });
 
     $('.header-btn').on('click', function () {
@@ -15,3 +14,23 @@ $(function () {
         $('.menu').removeClass('active');
     })
 });
+
+const openPopupButton = document.getElementById('popupOpen');
+const popupWindow = document.querySelector('.popup');
+const closeButton = document.querySelector('.close');
+const footerBtn = document.querySelector('.footer-btn');
+
+openPopupButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popupWindow.style.display = 'block';
+})
+footerBtn.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popupWindow.style.display = 'block';
+})
+
+closeButton.addEventListener('click', function () {
+    popupWindow.style.display = 'none';
+})
+
+
---
+++ js/second-slider.js
@@ -0,0 +1,12 @@
+$(function () {
+    $(".slider-bottom-items").slick({
+        arrows: true,
+        dots: false,
+        fade: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // autoplay: 3000,
+
+    });
+});
+
