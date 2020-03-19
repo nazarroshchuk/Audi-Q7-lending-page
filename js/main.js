41b8ecde6b6b10b6fce01c3c1a561b1f0629156c
nazarroshchuk <nazarroshchuk@gmail.com>
2020-03-19 19:21:51 +0200

    fix valid

--- js/main.js
+++ js/main.js
@@ -47,15 +47,39 @@ document.addEventListener('keydown', function (evt) {
 });
 
 //Validation Form inputs
-function validationForm() {
-    var inputName = document.getElementById('fname');
-    if (inputName.value === '') {
-        inputName.placeholder = '*Invalid type';
-        inputName.classList.add('invalid-color');
+// function validationForm() {
+//     var inputName = document.getElementById('fname');
+//     if (inputName.value === '') {
+//         inputName.placeholder = '*Invalid type';
+//         inputName.classList.add('invalid-color');
+//     }
+//     var inputLastName = document.getElementById('lname');
+//     if (inputLastName.value === '') {
+//         inputLastName.placeholder = '*Invalide type';
+//         inputLastName.classList.add('invalid-color');
+//         return console.log(inputLastName);
+//     }
+
+// }
+
+function checkEmtpyValue(inputForCheck, textValid, nameOfclass) {
+    if (inputForCheck.value === '') {
+        inputForCheck.placeholder = textValid;
+        inputForCheck.classList.add(nameOfclass);
     }
 }
-
-
+function validationForm() {
+    var inputName = document.getElementById('fname');
+    var inputLastName = document.getElementById('lname');
+    var inputTel = document.getElementById('tel');
+    checkEmtpyValue(inputName, '*Invalide type', 'invalid-color');
+    checkEmtpyValue(inputLastName, '*Invalide type', 'invalid-color');
+    // var str = inputTel.value.toString();
+    // if (str.lenght <= 14) {
+    //     checkEmtpyValue(inputTel, '*Invalide type', 'invalid-color');
+    // }
+    // console.log(inputTel.value.typeof());
+}
 
 var submitFormBtn = document.getElementById('submitBtnForm');
 submitFormBtn.addEventListener('click', validationForm);
